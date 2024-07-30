948deab1874a9285fc18c1e2197c452219e35928
Christoph Pirkl <4711730+kaklakariada@users.noreply.github.com>
2024-07-30 10:46:20 +0200

    #56: Upgrade dependencies (#57)
    * #56: Upgrade dependencies
    
    * Ignore compiler warnings about path
    
    * Adapt integration test

--- src/test/java/com/exasol/adapter/document/files/BucketfsDocumentFilesAdapterIT.java
+++ src/test/java/com/exasol/adapter/document/files/BucketfsDocumentFilesAdapterIT.java
@@ -3,7 +3,6 @@ package com.exasol.adapter.document.files;
 import static com.exasol.adapter.document.GenericUdfCallHandler.*;
 import static com.exasol.adapter.document.files.BucketfsDocumentFilesAdapter.ADAPTER_NAME;
 import static com.exasol.matcher.ResultSetStructureMatcher.table;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import java.io.*;
@@ -16,6 +15,7 @@ import java.util.function.Supplier;
 import org.junit.jupiter.api.*;
 
 import com.exasol.adapter.document.UdfEntryPoint;
+import com.exasol.bucketfs.Bucket;
 import com.exasol.bucketfs.BucketAccessException;
 import com.exasol.dbbuilder.dialects.DatabaseObject;
 import com.exasol.dbbuilder.dialects.exasol.*;
@@ -26,8 +26,8 @@ import com.exasol.udfdebugging.UdfTestSetup;
 
 @Tag("integration")
 class BucketfsDocumentFilesAdapterIT extends AbstractDocumentFilesAdapterIT {
-    private static final String ADAPTER_JAR = "document-files-virtual-schema-dist-8.0.4-bucketfs-2.0.3.jar";
-    private static final ExasolTestSetup EXASOL = new ExasolTestcontainerTestSetup();
+    private static final String ADAPTER_JAR = "document-files-virtual-schema-dist-8.1.2-bucketfs-2.0.4.jar";
+    private static final ExasolTestSetup EXASOL = ExasolTestcontainerTestSetup.start();
     private static final String BUCKETS_BFSDEFAULT_DEFAULT = "/buckets/bfsdefault/default/";
     private static UdfTestSetup udfTestSetup;
     private static ExasolObjectFactory testDbBuilder;
@@ -58,6 +58,11 @@ class BucketfsDocumentFilesAdapterIT extends AbstractDocumentFilesAdapterIT {
         udfTestSetup.close();
         statement.close();
         connection.close();
+    }
+
+    @Override
+    protected Bucket getBucketFSDefaultBucket() {
+        return EXASOL.getDefaultBucket();
     }
 
     @Override
@@ -126,15 +131,15 @@ class BucketfsDocumentFilesAdapterIT extends AbstractDocumentFilesAdapterIT {
                 .build());
     }
 
-    @Override
     @Test
-    public void testReadJson() throws SQLException, IOException {
-        // Override to verify source references column
+    @Override
+    public void testReadJson() throws IOException {
+        // Source reference column returns "/bfsdefault/default/1722320233480/testData-1.json" instead of
+        // "1722320233480/testData-1.json"
         createJsonVirtualSchema();
-        final ResultSet result = getStatement()
-                .executeQuery("SELECT ID, SOURCE_REFERENCE FROM TEST.BOOKS ORDER BY ID ASC;");
         final String expectedPrefix = "/bfsdefault/default/";
-        assertThat(result, table().row("book-1", allOf(startsWith(expectedPrefix), endsWith("/testData-1.json")))
-                .row("book-2", allOf(startsWith(expectedPrefix), endsWith("/testData-2.json"))).matches());
+        assertQuery("SELECT ID, SOURCE_REFERENCE FROM TEST.BOOKS ORDER BY ID ASC",
+                table().row("book-1", allOf(startsWith(expectedPrefix), endsWith("/testData-1.json")))
+                        .row("book-2", allOf(startsWith(expectedPrefix), endsWith("/testData-2.json"))).matches());
     }
 }
